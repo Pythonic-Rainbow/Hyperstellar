17c892f9deea4de9500898939e103ae27efdd7f8
Pythonic-Rainbow <wongchingho0@gmail.com>
2024-01-15 19:25:22 +0000

    [Bot] Completed Coc.cs refactor

--- Bot/Coc.cs
+++ Bot/Coc.cs
@@ -14,19 +14,41 @@ internal class Coc
 
     private class ClanUtil
     {
-        internal readonly Dictionary<string, ClanMember> Members = [];
         internal readonly Clan Clan;
-        internal readonly IEnumerable<ClanMember> existingMembers;
+        internal readonly Dictionary<string, ClanMember> Members = [];
+        internal readonly Dictionary<string, ClanMember> ExistingMembers = [];
+        internal readonly Dictionary<string, ClanMember> JoiningMembers = [];
+        internal readonly Dictionary<string, ClanMember> LeavingMembers;
 
         public ClanUtil(Clan clan)
         {
+            LeavingMembers = _prevClan.Members;
+            foreach (var member in clan.MemberList!)
+            {
+                Members[member.Tag] = member;
+                if (_prevClan.HasMember(member))
+                {
+                    ExistingMembers[member.Tag] = member;
+                    LeavingMembers.Remove(member.Tag);
+                } else
+                {
+                    JoiningMembers[member.Tag] = member;
+                }
+            }
+            Clan = clan;
+        }
+
+        internal ClanUtil(Clan clan, bool init)
+        {
+            LeavingMembers = [];
             foreach (var member in clan.MemberList!)
             {
                 Members[member.Tag] = member;
             }
             Clan = clan;
-            existingMembers = clan.MemberList!.Intersect(_prevClan.Clan.MemberList!, new MemberComparer());
         }
+
+        internal bool HasMember(ClanMember member) => Members.ContainsKey(member.Tag);
     }
 
     private class MemberComparer : IEqualityComparer<ClanMember>
@@ -52,15 +74,13 @@ internal class Coc
         if (clan.MemberList == null) return;
         ClanUtil clanUtil = new(clan);
         await CheckDonations(clanUtil);
-     
         _prevClan = clanUtil;
     }
 
     private static async Task CheckDonations(ClanUtil clan)
     {
         Dictionary<string, DonationTuple> donationsDelta = [];
-        var existingMemberTags = clan.existingMembers.Select(x => x.Tag);
-        foreach (var tag in existingMemberTags)
+        foreach (var tag in clan.ExistingMembers.Keys)
         {
             var current = clan.Members[tag];
             var previous = _prevClan.Members[tag];
@@ -75,7 +95,7 @@ internal class Coc
         }
     }
 
-    internal static async Task InitAsync() => _prevClan = new(await GetClanAsync());
+    internal static async Task InitAsync() => _prevClan = new(await GetClanAsync(), true);
 
     internal static async Task BotReadyAsync()
     {
