8420156b820d3bd7c31a9202f06bd990a1cd3487
Pythonic-Rainbow <wongchingho0@gmail.com>
2024-01-16 00:48:14 +0000

    Use single line namespace

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,12 +1,11 @@
-﻿namespace Hyperstellar
+﻿namespace Hyperstellar;
+
+public class Program
 {
-    public class Program
+    public static async Task Main()
     {
-        public static async Task Main()
-        {
-            await Discord.InitAsync();
-            await Coc.InitAsync();
-            await Task.Delay(-1);
-        }
+        await Discord.InitAsync();
+        await Coc.InitAsync();
+        await Task.Delay(-1);
     }
 }
