d7b15254317cfebc8bf3c64d027cfa9417a08471
Pythonic-Rainbow <wongchingho0@gmail.com>
2024-09-23 21:17:50 +0800

    [DPL] Don't download if already updated

--- Bot/hyperstellar.py
+++ Bot/hyperstellar.py
@@ -16,9 +16,14 @@ def explode(cond, msg):
         print('ERROR: ' + msg)
         os._exit(1)
 
+
 def build_artifact_url(run_id):
     return f'https://api.github.com/repos/TCLRainbow/Hyperstellar/actions/runs/{run_id}/artifacts'
 
+
+with open('hyperstellar-token.txt') as f:
+    token, current_title = f.readlines()
+    token = token.rstrip('\n')
 
 req = request.Request(RUNS_URL)
 with request.urlopen(req) as resp:
@@ -26,9 +31,13 @@ with request.urlopen(req) as resp:
 run_count = result['total_count']
 explode(run_count == 0, 'No workflow runs!')
 print(f'There are {run_count} runs')
+
 run = result['workflow_runs'][0]
-print(f'Checking run: {run["display_title"]}')
+gha_title = run['display_title']
+print(f'Checking run: {gha_title}')
 explode(run['status'] != 'completed', 'Run is not completed!')
+explode(gha_title == current_title, 'Already downloaded!')
+
 req = request.Request(run['jobs_url'])
 with request.urlopen(req) as resp:
     result = json.loads(resp.read())
@@ -42,6 +51,7 @@ for job in jobs:
 explode(not build_ok, 'No build job found!')
 explode(run['head_branch'] != 'main', 'Branch not main')
 explode(run['pull_requests'], 'Triggered by PR')
+
 run_id = run['id']
 print('Checking artifact')
 req = request.Request(build_artifact_url(run_id))
@@ -51,9 +61,8 @@ run_count = result['total_count']
 explode(run_count == 0, 'No artifacts found!')
 artifact = result['artifacts'][0]
 explode(artifact['name'] != 'Bot', 'Artifact name not Bot!')
+
 download_url = artifact['archive_download_url']
-with open('hyperstellar-token.txt') as f:
-    token = f.readline().rstrip('\n')
 req = request.Request(download_url, headers={'Authorization': f'Bearer {token}'})
 os.chdir('./Hyperstellar')
 print('Downloading')
@@ -65,5 +74,11 @@ with ZipFile('bot.zip') as f:
     f.extractall()
 print('Removing zip')
 os.remove('bot.zip')
+
+os.chdir('..')
+with open('hyperstellar-token.txt', 'w') as f:
+    f.write(f'{token}\n{gha_title}')
+print('Updated token.txt')
+
 print('Running')
 os.execv('Bot', ['./Bot'])
